0022389b10ca852e649b57ca8f611a83403012fd
Bruno Amaral <brunoamaralf@gmail.com>
2019-02-10 12:40:45 -0300

    Update .tf files to use 'dynamic blocks' compatible with tf-0_12+.

--- main.tf
+++ main.tf
@@ -1,130 +1,91 @@
-#######################
-# Launch configuration
-#######################
-resource "aws_launch_configuration" "this" {
-  count = "${var.create_lc}"
-
-  name_prefix                 = "${coalesce(var.lc_name, var.name)}-"
-  image_id                    = "${var.image_id}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.iam_instance_profile}"
-  key_name                    = "${var.key_name}"
-  security_groups             = ["${var.security_groups}"]
-  associate_public_ip_address = "${var.associate_public_ip_address}"
-  user_data                   = "${var.user_data}"
-  enable_monitoring           = "${var.enable_monitoring}"
-  spot_price                  = "${var.spot_price}"
-  placement_tenancy           = "${var.spot_price == "" ? var.placement_tenancy : ""}"
-  ebs_optimized               = "${var.ebs_optimized}"
-  ebs_block_device            = "${var.ebs_block_device}"
-  ephemeral_block_device      = "${var.ephemeral_block_device}"
-  root_block_device           = "${var.root_block_device}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-####################
-# Autoscaling group
-####################
 resource "aws_autoscaling_group" "this" {
-  count = "${var.create_asg && !var.create_asg_with_initial_lifecycle_hook ? 1 : 0}"
+  name                              = var.name
+  name_prefix                       = var.name_prefix
+  max_size                          = var.max_size
+  min_size                          = var.min_size
+  availability_zones                = var.availability_zones
+  default_cooldown                  = var.default_cooldown
+  launch_configuration              = var.launch_configuration
+  health_check_grace_period         = var.health_check_grace_period
+  health_check_type                 = var.health_check_type
+  desired_capacity                  = var.desired_capacity
+  force_delete                      = var.force_delete
+  load_balancers                    = var.load_balancers
+  vpc_zone_identifier               = var.vpc_zone_identifier
+  target_group_arns                 = var.target_group_arns
+  termination_policies              = var.termination_policies
+  suspended_processes               = var.suspended_processes 
+  placement_group                   = var.placement_group
+  metrics_granularity               = var.metrics_granularity
+  enabled_metrics                   = var.enabled_metrics
+  wait_for_capacity_timeout         = var.wait_for_capacity_timeout
+  min_elb_capacity                  = var.min_elb_capacity
+  wait_for_elb_capacity             = var.wait_for_elb_capacity
+  protect_from_scale_in             = var.protect_from_scale_in
+  service_linked_role_arn           = var.service_linked_role_arn
+  tags                              = var.tags
 
-  name_prefix          = "${join("-", compact(list(coalesce(var.asg_name, var.name), var.recreate_asg_when_lc_changes ? element(concat(random_pet.asg_name.*.id, list("")), 0) : "")))}-"
-  launch_configuration = "${var.create_lc ? element(concat(aws_launch_configuration.this.*.name, list("")), 0) : var.launch_configuration}"
-  vpc_zone_identifier  = ["${var.vpc_zone_identifier}"]
-  max_size             = "${var.max_size}"
-  min_size             = "${var.min_size}"
-  desired_capacity     = "${var.desired_capacity}"
+dynamic "tag" {
+  for_each = var.tag_inputs == null ? [] : var.tag_inputs
 
-  load_balancers            = ["${var.load_balancers}"]
-  health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type         = "${var.health_check_type}"
+  content {
+      key                    = tag.key
+      value                  = tag.value
+      propagate_at_launch    = tag.propagate_at_launch
+  }
+}  
+  
+//launch_template                   
+dynamic "launch_template" {
+  for_each = var.launch_template_inputs == null ? [] : var.launch_template_inputs
 
-  min_elb_capacity          = "${var.min_elb_capacity}"
-  wait_for_elb_capacity     = "${var.wait_for_elb_capacity}"
-  target_group_arns         = ["${var.target_group_arns}"]
-  default_cooldown          = "${var.default_cooldown}"
-  force_delete              = "${var.force_delete}"
-  termination_policies      = "${var.termination_policies}"
-  suspended_processes       = "${var.suspended_processes}"
-  placement_group           = "${var.placement_group}"
-  enabled_metrics           = ["${var.enabled_metrics}"]
-  metrics_granularity       = "${var.metrics_granularity}"
-  wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-  protect_from_scale_in     = "${var.protect_from_scale_in}"
-
-  tags = ["${concat(
-      list(map("key", "Name", "value", var.name, "propagate_at_launch", true)),
-      var.tags,
-      local.tags_asg_format
-   )}"]
-
-  lifecycle {
-    create_before_destroy = true
+  content {
+                      id         = launch_template.value.id 
+                      name       = launch_template.value.name
+                      version    = launch_template.value.version
   }
 }
+ 
+/* 
+//mixed_instances_policy           
+dynamic "mixed_instances_policy" {
+    for_each = var.mixed_instances_policy_inputs == null ? [] : var.mixed_instances_policy_inputs
 
-################################################
-# Autoscaling group with initial lifecycle hook
-################################################
-resource "aws_autoscaling_group" "this_with_initial_lifecycle_hook" {
-  count = "${var.create_asg && var.create_asg_with_initial_lifecycle_hook ? 1 : 0}"
-
-  name_prefix          = "${join("-", compact(list(coalesce(var.asg_name, var.name), var.recreate_asg_when_lc_changes ? element(concat(random_pet.asg_name.*.id, list("")), 0) : "")))}-"
-  launch_configuration = "${var.create_lc ? element(aws_launch_configuration.this.*.name, 0) : var.launch_configuration}"
-  vpc_zone_identifier  = ["${var.vpc_zone_identifier}"]
-  max_size             = "${var.max_size}"
-  min_size             = "${var.min_size}"
-  desired_capacity     = "${var.desired_capacity}"
-
-  load_balancers            = ["${var.load_balancers}"]
-  health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type         = "${var.health_check_type}"
-
-  min_elb_capacity          = "${var.min_elb_capacity}"
-  wait_for_elb_capacity     = "${var.wait_for_elb_capacity}"
-  target_group_arns         = ["${var.target_group_arns}"]
-  default_cooldown          = "${var.default_cooldown}"
-  force_delete              = "${var.force_delete}"
-  termination_policies      = "${var.termination_policies}"
-  suspended_processes       = "${var.suspended_processes}"
-  placement_group           = "${var.placement_group}"
-  enabled_metrics           = ["${var.enabled_metrics}"]
-  metrics_granularity       = "${var.metrics_granularity}"
-  wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-  protect_from_scale_in     = "${var.protect_from_scale_in}"
-
-  initial_lifecycle_hook {
-    name                    = "${var.initial_lifecycle_hook_name}"
-    lifecycle_transition    = "${var.initial_lifecycle_hook_lifecycle_transition}"
-    notification_metadata   = "${var.initial_lifecycle_hook_notification_metadata}"
-    heartbeat_timeout       = "${var.initial_lifecycle_hook_heartbeat_timeout}"
-    notification_target_arn = "${var.initial_lifecycle_hook_notification_target_arn}"
-    role_arn                = "${var.initial_lifecycle_hook_role_arn}"
-    default_result          = "${var.initial_lifecycle_hook_default_result}"
-  }
-
-  tags = ["${concat(
-      list(map("key", "Name", "value", var.name, "propagate_at_launch", true)),
-      var.tags,
-      local.tags_asg_format
-   )}"]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+    content {
+              dynamic "instances_distribution" {
+                for_each = mixed_instances_policy.value.instances_distribution_inputs == null ? [] : mixed_instances_policy.value.instances_distribution_inputs
+                content {
+                  on_demand_allocation_strategy             = instances_distribution.value.on_demand_allocation_strategy
+                  on_demand_base_capacity                   = instances_distribution.value.on_demand_base_capacity
+                  on_demand_percentage_above_base_capacity  = instances_distribution.value.on_demand_percentage_above_base_capacity
+                  spot_allocation_strategy                  = instances_distribution.value.spot_allocation_strategy
+                  spot_instance_pools                       = instances_distribution.value.spot_instance_pools
+                  spot_max_price                            = instances_distribution.value.spot_max_price
+                }
+              }
+              dynamic "launch_template" {
+                for_each = mixed_instances_policy.value.launch_template_inputs == null ? [] : mixed_instances_policy.value.launch_template_inputs
+                content {
+                  dynamic "launch_template_specification" {
+                    for_each = launch_template.value.launch_template_specification_inputs == null ? [] : launch_template.value.launch_template_specification_inputs
+                    content {
+                      launch_template_id                      = launch_template_specification.value.launch_template_id 
+                      launch_template_name                    = launch_template_specification.value.launch_template_name
+                      version                                 = launch_template_specification.value.version
+                    }
+                  }
+                  dynamic "override" {
+                    for_each = launch_template.value.override_inputs == null ? [] : launch_template.value.override_inputs
+                    content {
+                      instance_type                          = override.value.instance_type
+                    }
+                  }
+                }
+              }
+            }
+}
+*/
 }
 
-resource "random_pet" "asg_name" {
-  count = "${var.recreate_asg_when_lc_changes ? 1 : 0}"
 
-  separator = "-"
-  length    = 2
 
-  keepers = {
-    # Generate a new pet name each time we switch launch configuration
-    lc_name = "${var.create_lc ? element(concat(aws_launch_configuration.this.*.name, list("")), 0) : var.launch_configuration}"
-  }
-}
